e6df2319e2f89b42146de95d4de11581828e2bad
Steven Thompson <sthompson02@gmail.com>
2017-01-16 18:16:38 +1100

    Website Backup
    No longer supporting thompsonwebdesigns.com

---
+++ js/mobmenu.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$( "#mobilemenubutton" ).click(function() {
+  $( "#mobilemenu" ).toggleClass( "show" );
+  //console.log("test");
+});
+
+$( "#cross" ).click(function() {
+  $( "#mobilemenu" ).toggleClass( "show" );
+});
